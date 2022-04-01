e18e6bc21e1cea8d1554b816cc28ddd7a2186adb
Troy Witmer <91856253+T-Witmer@users.noreply.github.com>
2022-03-31 21:51:12 -0400

    aa

--- PythonScripts/Python/regexLab/regxLab.py
+++ PythonScripts/Python/regexLab/regxLab.py
@@ -1,2 +1,17 @@
 # Troy Witmer
 
+import re
+
+text = open("words.txt", "r")
+text = text.read()
+
+def catdog(text):
+    cat = r"[C|c][A|a][T|t]"
+    dog = r"[D|d][O|o][G|g]"
+    catoutput = re.findall(cat,text)
+    dogoutput = re.findall(dog,text)
+    output = len(catoutput) + len(dogoutput)
+
+    return output
+
+print(catdog(text))
